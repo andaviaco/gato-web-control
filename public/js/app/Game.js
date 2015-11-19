df4a0ff5ff68148520046bb40d8002e2aec0d77a
Andrés Ávila <andpy.me@gmail.com>
2015-11-19 05:55:42 -0600

    fix Game 'class'

--- public/js/app/Game.js
+++ public/js/app/Game.js
@@ -1,52 +1,53 @@
 import _ from 'underscore';
 
-var table = [
-    [
-        {
-            key: 0,
-            player: ''
-        },
-        {
-            key: 1,
-            player: ''
-        },
-        {
-            key: 2,
-            player: ''
-        }
-    ],
-    [
-        {
-            key: 3,
-            player: ''
-        },
-        {
-            key: 4,
-            player: ''
-        },
-        {
-            key: 5,
-            player: ''
-        }
-    ],
-    [
-        {
-            key: 6,
-            player: ''
-        },
-        {
-            key: 7,
-            player: ''
-        },
-        {
-            key: 8,
-            player: ''
-        }
-    ]
-];
+var Game = function () {
+    var table = [
+        [
+            {
+                key: 0,
+                player: ''
+            },
+            {
+                key: 1,
+                player: ''
+            },
+            {
+                key: 2,
+                player: ''
+            }
+        ],
+        [
+            {
+                key: 3,
+                player: ''
+            },
+            {
+                key: 4,
+                player: ''
+            },
+            {
+                key: 5,
+                player: ''
+            }
+        ],
+        [
+            {
+                key: 6,
+                player: ''
+            },
+            {
+                key: 7,
+                player: ''
+            },
+            {
+                key: 8,
+                player: ''
+            }
+        ]
+    ];
 
-module.exports = function() {
-    return {
-        table: table
-    };
+
+    this.table = table;
 };
+
+module.exports = Game;
